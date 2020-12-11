8e868e420968f6f1c29aef514bf852a58dab32dd
gaboesquivel <contact@gaboesquivel.com>
2020-12-11 13:22:53 -0600

    fix: createContextHook export

--- packages/hooks-utils/src/index.tsx
+++ packages/hooks-utils/src/index.tsx
@@ -1,3 +1,3 @@
-export { default as createContextHook } from './utils/createContextHook'
+export { createContextHook } from './utils/createContextHook'
 export { default as useAsyncCallback } from '@blockmatic/use-async-callback'
 export { default as useIsMounted } from '@blockmatic/use-is-mounted'
