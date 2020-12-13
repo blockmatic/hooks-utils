34c1ed7e3538a8a467473cd325093c02b1e85c17
gaboesquivel <contact@gaboesquivel.com>
2020-12-13 12:56:47 -0600

    chore: refactor createContextHook

--- packages/hooks-utils/src/utils/createContextHook.tsx
+++ packages/hooks-utils/src/utils/createContextHook.tsx
@@ -1,30 +1,31 @@
 import React from 'react'
 
-export interface ContainerProviderProps<State = void> {
-  initialState?: State
+export interface HookProviderProps<Params> {
+  params?: Params
 }
-export type Container<Value, State = void> = [() => Value, React.ComponentType<ContainerProviderProps<State>>]
+
+export type ContextHook<Value, Params> = [() => Value, ContextHookProvider<Params>]
 
 const EMPTY: unique symbol = Symbol('__EMPTY__')
 
-export const createContextHook = <Value, State = void>(
-  useHook: (initialState?: State) => Value,
+export type ContextHookProvider<State> = React.FC<HookProviderProps<State>>
+
+export const createContextHook = <Value, Params>(
+  useHook: (params?: Params) => Value,
   errorMsg = 'Hook must be wrapped with <Provider/>',
-): Container<Value, State> => {
+): ContextHook<Value, Params> => {
   const Context = React.createContext<Value | typeof EMPTY>(EMPTY)
 
-  const Provider: React.FC<ContainerProviderProps<State>> = ({ initialState, children }) => {
-    const value = useHook(initialState)
+  const Provider: ContextHookProvider<Params> = ({ params, children }) => {
+    const value = useHook(params)
     return <Context.Provider value={value}>{children}</Context.Provider>
   }
 
-  const useContainer = (): Value => {
+  const useContextHook = (): Value => {
     const value = React.useContext(Context)
-    if (value === EMPTY) {
-      throw new Error(errorMsg)
-    }
+    if (value === EMPTY) throw new Error(errorMsg)
     return value
   }
 
-  return [useContainer, Provider]
+  return [useContextHook, Provider]
 }
