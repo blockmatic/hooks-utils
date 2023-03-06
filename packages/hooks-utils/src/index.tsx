00947451491f544221809105f93a47cbe3cd09ea
Gabo Esquivel <contact@gaboesquivel.com>
2023-03-06 10:05:33 -0600

    feat: upgrade to react 18

--- packages/hooks-utils/src/index.tsx
+++ packages/hooks-utils/src/index.tsx
@@ -1,3 +1,1 @@
 export { createContextHook } from './utils/createContextHook'
-export { default as useAsyncCallback } from '@blockmatic/use-async-callback'
-export { default as useIsMounted } from '@blockmatic/use-is-mounted'
--- packages/hooks-utils/src/utils/createContextHook.tsx
+++ packages/hooks-utils/src/utils/createContextHook.tsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export interface HookProviderProps<Params> {
-  params?: Params
+  params?: Params;
+  children: React.ReactNode;
 }
-export type ContextHookProvider<State> = React.FC<HookProviderProps<State>>
+export type ContextHookProvider<State> = React.FC<HookProviderProps<State>>;
 
-export type ContextHook<Value, Params> = [() => Value, ContextHookProvider<Params>]
+export type ContextHook<Value, Params> = [() => Value, ContextHookProvider<Params>];
 
-const EMPTY: unique symbol = Symbol('__EMPTY__')
+const EMPTY: unique symbol = Symbol("__EMPTY__");
 
 export const createContextHook = <Value, Params>(
   useHook: (params?: Params) => Value,
-  errorMsg = 'Hook must be wrapped with <Provider/>',
+  errorMsg = "Hook must be wrapped with <Provider/>"
 ): ContextHook<Value, Params> => {
-  const Context = React.createContext<Value | typeof EMPTY>(EMPTY)
+  const Context = React.createContext<Value | typeof EMPTY>(EMPTY);
 
   const Provider: ContextHookProvider<Params> = ({ params, children }) => {
-    const value = useHook(params)
-    return <Context.Provider value={value}>{children}</Context.Provider>
-  }
+    const value = useHook(params);
+    return <Context.Provider value={value}>{children}</Context.Provider>;
+  };
 
   const useContextHook = (): Value => {
-    const value = React.useContext(Context)
-    if (value === EMPTY) throw new Error(errorMsg)
-    return value
-  }
+    const value = React.useContext(Context);
+    if (value === EMPTY) throw new Error(errorMsg);
+    return value;
+  };
 
-  return [useContextHook, Provider]
-}
+  return [useContextHook, Provider];
+};
